bd5f20cb7e056ee1d8281e0a3ec2afea29823ea3
Furkan ÇAĞLAR <caglar.furkann91@gmail.com>
2022-11-04 15:59:11 +0300

    Added some changes in Jetpackt Navigation

--- app/src/main/java/io/example/exemplaryproject/MainActivity.kt
+++ app/src/main/java/io/example/exemplaryproject/MainActivity.kt
@@ -1,13 +1,20 @@
 package io.example.exemplaryproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        println("denemeee :)")
     }
 }
